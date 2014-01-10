26f21033d779e3e61e69a3edc05a057eee9c3ace
Boyuan Zhang <zhang_boyuan@hotmail.com>
2014-01-09 19:27:20 -0500

    Included css loader in handler array

--- BoyuanZhangNodeWebsite/index.js
+++ BoyuanZhangNodeWebsite/index.js
@@ -1,14 +1,17 @@
-var server = require( "./Server/server");
-var requestHandler = require( "./Modules/requestHandler");
-var router = require( "./Modules/router");
+var server = require( "./Server/server"),
+	requestHandler = require( "./Modules/requestHandler"),
+	router = require( "./Modules/router");
 
 //Handler to different pages
 var handler = new Array();
-handler["/"] = requestHandler.start;
-handler["/start"] = requestHandler.start;
+handler["/"] = requestHandler.home;
+handler["/home"] = requestHandler.home;
 handler["/resume"] = requestHandler.resume;
 handler["/blog"] = requestHandler.blog;
 handler["/projects"] = requestHandler.projects;
 handler["/contact"] = requestHandler.contact;
+
+//For loading css / js files
+handler[".css"] = requestHandler.loadCSS;
 
 server.start(router.route, handler);
